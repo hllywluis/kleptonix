066e60520c34c23f5a1516ed94f5707c90cc70e9
Luis Bauza <hllywluis2@me.com>
2022-01-18 11:31:23 -0500

    Add form for user sign in in sign_in.js

--- pages/sign_in.js
+++ pages/sign_in.js
@@ -1,7 +1,9 @@
 import Head from "next/head"
 
 import Alert from "react-bootstrap/Alert"
+import Button from "react-bootstrap/Button"
 import Container from "react-bootstrap/Container"
+import Form from "react-bootstrap/Form"
 import KleptonixNavbar from "../components/navbar"
 
 export default SignIn => {
@@ -29,6 +31,23 @@ export default SignIn => {
             <Container className="text-center pt-2">
                 <h1 style={linkStyle}>Sign in with your <span style={brandStyle}>Kleptonix</span> account.</h1>
                 <h4 style={brandStyle} className="text-muted">Access all the things.</h4>
+            </Container>
+            <Container className="pt-5">
+                <Form>
+                    <Form.Group className="mb-3" controlId="sign_up_email">
+                        <Form.Label style={brandStyle}>Email address</Form.Label>
+                        <Form.Control name="email" type="email" placeholder="luis@kleptonix.com" required />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="sign_up_password">
+                        <Form.Label style={brandStyle}>Password</Form.Label>
+                        <Form.Control name="password" type="password" placeholder="Password" required />
+                    </Form.Group>
+                    <Container className="text-center pt-2">
+                        <Button style={brandStyle} variant="dark" type="submit">
+                            Sign In
+                        </Button>
+                    </Container>
+                </Form>
             </Container>
         </>
     )
